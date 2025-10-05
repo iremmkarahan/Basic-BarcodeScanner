21a8e7863480d1564715fd455f178bc74cb0244c
İrem Karahan <irem.karahan@tedu.edu.tr>
2025-10-05 10:46:17 +0300

    working on camera setup for scanning

--- BarcodeScanner/BarcodeScanner/ScannerVC.swift
+++ BarcodeScanner/BarcodeScanner/ScannerVC.swift
@@ -5,71 +5,91 @@
 //  Created by İreemmmm on 30.09.2025.
 //
 
+
 import AVFoundation
 import UIKit
 
+enum CameraError: String {
+    case invalidDeviceInput = "Something is wrong with the device's camera."
+    case invalidScannedValue = "The value scanned is not valid. This app scans EAN8, EAN13, UPC and QR codes."
+}
+
 protocol ScannerVCDelegate: AnyObject {
     func didFind(barcode: String)
+    func didSurface(error: CameraError)
 }
 
 final class ScannerVC: UIViewController {
-    
-    let captureSession = AVCaptureSession()
-    var previewLayer: AVCaptureVideoPreviewLayer?
-    weak var scannerDelegate: ScannerVCDelegate!
-    
-    init(scannerDelegate: ScannerVCDelegate){
-        super.init(nibName: nil, bundle: nil)
+
+    private let captureSession = AVCaptureSession()
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerVCDelegate?
+
+    init(scannerDelegate: ScannerVCDelegate) {
         self.scannerDelegate = scannerDelegate
+        super.init(nibName: nil, bundle: nil)
     }
-    
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented")}
-    
+
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        setupCaptureSession()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer?.frame = view.bounds
+    }
+
     private func setupCaptureSession() {
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
-            return
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            configureSession()
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    DispatchQueue.main.async { self.configureSession() }
+                }
+            }
+        default:
+            print("Camera access denied or restricted")
         }
-        let videoInput: AVCaptureDeviceInput
-        
-        do {
-            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {
-            return
-        }
-        
-        if captureSession.canAddInput(videoInput){
-            captureSession.addInput(videoInput)
-            
-        } else {
-            return
-        }
-        
-        let metaDataOutput = AVCaptureMetaDataOutput()
-        
-        if captureSession.canAddOutput(metaDataOutput) {
-            captureSession.addOutput(metaDataOutput)
-            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
-        } else {
-            return
-        }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer!.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer!)
-        
-        
-        
     }
-    
+
+    private func configureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video),
+              let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice),
+              captureSession.canAddInput(videoInput) else { return }
+
+        captureSession.addInput(videoInput)
+
+        let metaDataOutput = AVCaptureMetadataOutput()
+        guard captureSession.canAddOutput(metaDataOutput) else { return }
+        captureSession.addOutput(metaDataOutput)
+
+        metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        metaDataOutput.metadataObjectTypes = [.ean8, .ean13, .upce, .qr]
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+
+        captureSession.startRunning()
+    }
 }
 
 extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject {
-            if metadataObject.type == .qr {
-                scannerDelegate.didFind(barcode: metadataObject.stringValue ?? "")
-            }
-        }
+    func metadataOutput(_ output: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
+
+        guard let object = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+              let barcode = object.stringValue else { return }
+
+        captureSession.stopRunning()
+        scannerDelegate?.didFind(barcode: barcode)
+        dismiss(animated: true)
     }
 }
